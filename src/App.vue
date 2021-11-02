7d557dbccb39748f5d0081085eb765670da0a04e
DJ Lee <dog1448@gmail.com>
2021-11-03 01:59:14 +0900

    firebase init

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,61 @@
       color="primary"
       dark
     >
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-toolbar-title>Page title</v-toolbar-title>
-
-    <v-spacer></v-spacer>
-
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+        <site-title :title="title"></site-title>
+        <v-spacer></v-spacer>
         <v-btn icon to="/about">
           <v-icon>mdi-magnify</v-icon>
         </v-btn>
-
         <v-btn icon to="/">
           <v-icon>mdi-heart</v-icon>
         </v-btn>
-
         <v-btn icon>
           <v-icon>mdi-dots-vertical</v-icon>
         </v-btn>
     </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+      <site-menu></site-menu>
+    </v-navigation-drawer>
     <v-content>
       <router-view></router-view>
     </v-content>
-    <v-footer app color="primary" dark absolute>
-      <v-spacer></v-spacer>
-      <div>&copy; {{ new Date().getFullYear() }}</div>
-    </v-footer>
+    <site-footer :footer="footer"></site-footer>
   </v-app>
 </template>
 
 <script>
+import SiteTitle from '@/views/site/title'
+import SiteFooter from '@/views/site/footer'
+import SiteMenu from '@/views/site/menu'
 
 export default {
-  name: 'App'
+
+  components: { SiteTitle, SiteFooter, SiteMenu },
+  name: 'App',
+  data () {
+    return {
+      drawer: false,
+      title: '나는 타이틀입니다.',
+      footer: '푸터입니다.',
+      items: [
+        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+        { title: 'Photos', icon: 'mdi-image' },
+        { title: 'About', icon: 'mdi-help-box' }
+      ]
+    }
+  }
 }
 </script>
